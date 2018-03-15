ca3617878d04b57513d22cb8d1194698b7d7332f
Miguel Costa <miguel.m.costa15@gmail.com>
2018-03-15 16:37:38 +0000

    add inicial files

---
+++ adhoc_app/src/Adhoc_app.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.net.*;
+
+public class Adhoc_app implements Runnable {
+
+        public void run() {
+
+            try {
+                System.out.println(" Server is Running  ");
+                ServerSocket ss = new ServerSocket(9999);
+
+                while (true) {
+
+                    Socket cs = ss.accept();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+                    PrintWriter writer= new PrintWriter(cs.getOutputStream());
+
+                    String data1 = reader.readLine().trim();
+                    String data2 = reader.readLine().trim();
+
+                    int num1 = Integer.parseInt(data1);
+                    int num2 = Integer.parseInt(data2);
+
+                    int result = num1 + num2;
+
+
+                    cs.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+        }
+
+    public static void main(String argv[]) throws Exception {
+        (new Thread(new Adhoc_app())).start();
+    }
+
+}
---
+++ adhoc_app/src/Client.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String argv[])
+    {
+        try{
+            Socket socketClient= new Socket("localhost",9999);
+            System.out.println("Client: "+"Connection Established");
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            PrintWriter writer= new PrintWriter(socketClient.getOutputStream());
+
+            String msg;
+            msg = reader.readLine();
+            while (!msg.equals("bye")){
+                System.out.println(msg);
+                msg = reader.readLine();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
