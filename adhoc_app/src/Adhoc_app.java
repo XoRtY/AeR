2119f50d24a7ac31be65036799a6642f0e8c819d
MatiasN <matias.nicolau@gmail.com>
2018-03-22 16:59:35 +0000

    StartedUDP stuff

--- adhoc_app/src/Adhoc_app.java
+++ adhoc_app/src/Adhoc_app.java
@@ -8,10 +8,16 @@ public class Adhoc_app implements Runnable {
             try {
                 System.out.println(" Server is Running  ");
                 ServerSocket ss = new ServerSocket(9999);
+                DatagramSocket ds = new DatagramSocket(9999);
+
 
                 while (true) {
 
                     Socket cs = ss.accept();
+                    byte[] receiveData = new byte[1024];
+                    byte[] sendData = new byte[1024];
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    ds.receive(receivePacket);
 
                     BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
                     PrintWriter writer= new PrintWriter(cs.getOutputStream());
