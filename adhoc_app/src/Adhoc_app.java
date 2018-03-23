7986fe980e7e4558c646c1d9970e557d75f666f1
MatiasN <matias.nicolau@gmail.com>
2018-03-23 20:53:59 +0000

    More udp stuff

--- adhoc_app/src/Adhoc_app.java
+++ adhoc_app/src/Adhoc_app.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.TreeMap;
+import java.lang.String;
+
+import static java.lang.Integer.parseInt;
 
 public class Adhoc_app implements Runnable {
 
@@ -13,25 +17,44 @@ public class Adhoc_app implements Runnable {
 
                 while (true) {
 
-                    Socket cs = ss.accept();
+                    TreeMap <Inet6Address, Inet6Address> table= new TreeMap <Inet6Address,Inet6Address>();
+
+                    //Socket cs = ss.accept();
                     byte[] receiveData = new byte[1024];
                     byte[] sendData = new byte[1024];
-                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                    ds.receive(receivePacket);
+                    InetAddress IPAddress = InetAddress.getByName("localhost");
 
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+                    //Send
+                    String toSend = "H1";
+                    sendData = toSend.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9999);
+                    ds.send(sendPacket);
+
+                    //Receive
+                    DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
+                    ds.receive(receivedPacket);
+                    String msg = new String(receivedPacket.getData(), receivedPacket.getOffset(),
+                            receivedPacket.getLength());
+                    String type = msg.substring(0,1);
+                    String ttlString = msg.substring(1,2);
+                    int ttl = parseInt(ttlString);
+
+
+
+
+                    /*BufferedReader reader = new BufferedReader(new InputStreamReader(cs.getInputStream()));
                     PrintWriter writer= new PrintWriter(cs.getOutputStream());
 
                     String data1 = reader.readLine().trim();
                     String data2 = reader.readLine().trim();
 
-                    int num1 = Integer.parseInt(data1);
+                    int num1 = Integer.parseInt(data1);     Não percebo o objetivo deste pedaço de código - Matias
                     int num2 = Integer.parseInt(data2);
 
-                    int result = num1 + num2;
+                    int result = num1 + num2;*/
 
 
-                    cs.close();
+                    //cs.close();
                 }
             } catch (IOException e) {
                 System.out.println(e);
