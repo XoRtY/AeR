0367f9f55da0fef340a4c3957f05f7f983bb947f
Matias Araújo <a76234@alunos.uminho.pt>
2018-04-01 11:56:16 +0100

    Comentários everywhere

--- adhoc_app/src/Adhoc_app.java
+++ adhoc_app/src/Adhoc_app.java
@@ -10,7 +10,7 @@ import static java.lang.Integer.parseInt;
 
 public class Adhoc_app implements Runnable {
 
-        Set<InetAddress> parseToSend(TreeMap<InetAddress, InetAddress> table){
+        Set<InetAddress> parseToSend(TreeMap<InetAddress, InetAddress> table){        // Cria um set com todos os targets a 1 de distância
             TreeMap<InetAddress, InetAddress> parsed = new TreeMap<>();
             for (Map.Entry<InetAddress,InetAddress>  entry: table.entrySet()){
                 if(entry.getKey().equals(entry.getValue())){
@@ -24,9 +24,9 @@ public class Adhoc_app implements Runnable {
         public void run() {
 
             Scanner inVars = new Scanner(System.in);
-            System.out.println("Hello Interval in seconds: ");
+            System.out.println("Hello Interval in seconds: "); //Tempo entre cada hello
             int helloInterval = inVars.nextInt();
-            System.out.println("Dead Interval in seconds: ");
+            System.out.println("Dead Interval in seconds: ");  //tempo que fica a espera de um hello
             int deadInterval = inVars.nextInt();
 
             try {
@@ -37,7 +37,7 @@ public class Adhoc_app implements Runnable {
 
                 while (true) {
 
-                    Thread.sleep(helloInterval*1000);
+                    Thread.sleep(helloInterval*1000); //Espera o tempo entre Hellos
 
                     TreeMap<InetAddress,InetAddress> table = new TreeMap<>();
 
@@ -46,32 +46,32 @@ public class Adhoc_app implements Runnable {
                     //byte[] sendData = new byte[1024];
 
                     //Join multicast group
-                    InetAddress group = InetAddress.getByName("FF02::1");
-                    Inet6Address IPAddress = (Inet6Address) Inet6Address.getByName("FF02::1");
+                    InetAddress group = InetAddress.getByName("FF02::1"); //Ip do grupo multicast
+                    Inet6Address IPAddress = (Inet6Address) Inet6Address.getByName("FF02::1");  //Ip do próprio
                     ms.joinGroup(group);
 
                     //Send, utiliza o datagram socket para enviar
                     //String tableString = table.toString();
                     //String toSend = "H1"+tableString;
                     //sendData = toSend.getBytes();
-                    Set <InetAddress> parsedKeySet = parseToSend(table);
-                    HelloPacket data = new HelloPacket(parsedKeySet);
+                    Set <InetAddress> parsedKeySet = parseToSend(table);  // Prepara o set com os nós a 1 de distancia para enviar por udp
+                    HelloPacket data = new HelloPacket(parsedKeySet);     // Cria um objeto com o set
 
-                    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-                    ObjectOutputStream sendData = new ObjectOutputStream(byteOut);
-                    sendData.writeObject(data);
-                    sendData.flush();
-                    byte[] sendDataBytes = byteOut.toByteArray();
-                    DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, IPAddress, 9999);
-                    ds.send(sendPacket);
+                    ByteArrayOutputStream byteOut = new ByteArrayOutputStream();           //
+                    ObjectOutputStream sendData = new ObjectOutputStream(byteOut);         //
+                    sendData.writeObject(data);                                            // Serializa o objeto para o poder enviar
+                    sendData.flush();                                                      //
+                    byte[] sendDataBytes = byteOut.toByteArray();                          //
+                    DatagramPacket sendPacket = new DatagramPacket(sendDataBytes, sendDataBytes.length, IPAddress, 9999);  // Prepara o pacote
+                    ds.send(sendPacket);   //Envia o pacote
 
                     //Receive, utiliza o multicast socket para receber
-                    DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);
-                    ms.receive(receivedPacket);
-                    ByteArrayInputStream bis = new ByteArrayInputStream(receiveData);
-                    ObjectInput in = null;
-                    in = new ObjectInputStream(bis);
-                    Object o = in.readObject();
+                    DatagramPacket receivedPacket = new DatagramPacket(receiveData, receiveData.length);  // Prepara um objeto para receber o pacote
+                    ms.receive(receivedPacket);  // Recebe o pacote
+                    ByteArrayInputStream bis = new ByteArrayInputStream(receiveData); //
+                    ObjectInput in = null;                                            // De-serializa o objeto que vem no pacote
+                    in = new ObjectInputStream(bis);                                  //
+                    Object o = in.readObject();                                       //
 
                     /*String msg = new String(receivedPacket.getData(), receivedPacket.getOffset(),
                             receivedPacket.getLength());
@@ -80,16 +80,16 @@ public class Adhoc_app implements Runnable {
                     String peerTableString = msg.substring(2);
                     int ttl = parseInt(ttlString);*/
 
-                    if(o instanceof HelloPacket){
-                        HelloPacket received = (HelloPacket) o;
-                        InetAddress from = receivedPacket.getAddress();
-                        if(table.containsKey(from) && !table.get(from).equals(from)){
+                    if(o instanceof HelloPacket){                                    // Caso o objeto que vem no pacote seja um Hello packet
+                        HelloPacket received = (HelloPacket) o;                      // Parse para hello packet
+                        InetAddress from = receivedPacket.getAddress();              // IP do transmitter
+                        if(table.containsKey(from) && !table.get(from).equals(from)){   //Caso já tenha este target na tabela de encaminhamento mas tiver mais de 1 de distancia
                             table.replace(from,from);
                         }
-                        if(!table.containsKey(from)){
+                        if(!table.containsKey(from)){                                   //Caso não tenha este target na tabela de encaminhamento
                             table.put(from,from);
                         }
-                        Set<InetAddress> peerKeySet = received.getPeers();
+                        Set<InetAddress> peerKeySet = received.getPeers();              //Pega no set com os targets e adiciona-os, pondo como prox salto o router que enviou o pacote
                         for(InetAddress entry : peerKeySet){
                             if(!table.containsKey(entry)){
                                 table.put(entry,from);
